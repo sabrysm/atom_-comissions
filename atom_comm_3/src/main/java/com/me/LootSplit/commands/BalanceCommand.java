0941389e3428aa330a01a9b95bd77113dec5a1a4
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-09-25 19:49:57 +0300

    Added Balance command

--- atom_comm_3/src/main/java/com/me/LootSplit/commands/BalanceCommand.java
+++ atom_comm_3/src/main/java/com/me/LootSplit/commands/BalanceCommand.java
@@ -1,4 +1,42 @@
 package com.me.LootSplit.commands;
 
-public class BalanceCommand {
+import com.me.LootSplit.database.DatabaseManager;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLException;
+
+public class BalanceCommand implements ISlashCommand {
+    @NotNull
+    @Override
+    public CommandData getCommandData() {
+        return Commands.slash("bal", "Check your balance")
+                .setGuildOnly(true);
+    }
+
+    @Override
+    public void execute(@NotNull SlashCommandInteractionEvent event) {
+        try {
+            long userId = Long.parseLong(event.getUser().getId());
+            DatabaseManager databaseManager = new DatabaseManager();
+            long balance = databaseManager.getPlayerBalance(userId);
+            sendBalanceMessage(event, balance);
+        } catch (SQLException e) {
+            System.out.println("Error getting balance: " + e.getMessage());
+        }
+    }
+
+    // Messages
+
+    public static void sendBalanceMessage(@NotNull SlashCommandInteractionEvent event, long balance) {
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Balance");
+        embedBuilder.setDescription("Your balance is: " + balance);
+        embedBuilder.setColor(0x00FF00);
+        event.replyEmbeds(embedBuilder.build()).queue();
+    }
 }
