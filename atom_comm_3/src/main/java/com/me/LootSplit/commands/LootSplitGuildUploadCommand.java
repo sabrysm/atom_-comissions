2ee1058bbd13a8b712eb54373a2987d4c44bb143
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-09-26 13:38:10 +0300

    Added  command and helper for LootSplitGuildUpload

--- atom_comm_3/src/main/java/com/me/LootSplit/commands/LootSplitGuildUploadCommand.java
+++ atom_comm_3/src/main/java/com/me/LootSplit/commands/LootSplitGuildUploadCommand.java
@@ -1,24 +1,97 @@
 package com.me.LootSplit.commands;
 
-import com.me.LootSplit.database.DatabaseManager;
-import com.me.LootSplit.utils.CTA;
-import com.me.LootSplit.commands.ISlashSubCommand;
+import com.me.LootSplit.utils.GuildUploadHelper;
+import com.me.LootSplit.utils.LootSplitGuildUploadHelper;
+import com.me.LootSplit.utils.LootSplitSession;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.sql.SQLException;
+import java.util.concurrent.CompletableFuture;
 
-public class LootSplitGuildUploadCommand implements ISlashSubCommand {
+import static com.me.LootSplit.utils.Messages.sendErrorProvideTextOptionMessage;
+import static com.me.LootSplit.utils.Messages.sendErrorOnlyOneOptionAllowedMessage;
+import static com.me.LootSplit.utils.Messages.sendErrorConnectingDatabaseMessage;
+import static com.me.LootSplit.utils.Messages.sendErrorUploadMessage;
+import static com.me.LootSplit.utils.Messages.sendErrorReadingMessage;
+import static com.me.LootSplit.utils.Messages.sendNotGuildListMessage;
+
+public class LootSplitGuildUploadCommand implements ISlashSubCommand{
+    private CompletableFuture<Void> cf;
+    private String lootSplitId;
+
     @Override
-    public void execute(@NotNull SlashCommandInteractionEvent event) {
-        try {
+    public void execute(SlashCommandInteractionEvent event) {
+        File textFile;
 
-        } catch (Exception e) {
+        try {
+            lootSplitId = LootSplitSession.getLootSplitIdFromGuild(event.getGuild().getIdLong());
+        } catch (SQLException e) {
             e.printStackTrace();
+            sendErrorConnectingDatabaseMessage(event);
+            return;
+        }
+        if (event.getOption("text") == null && event.getOption("file") == null) {
+            sendErrorProvideTextOptionMessage(event);
+            return;
+        } else if (event.getOption("text") != null && event.getOption("file") != null) {
+            sendErrorOnlyOneOptionAllowedMessage(event);
+            return;
+        } else if (event.getOption("text") != null) {
+            String text = event.getOption("text").getAsString();
+            try {
+                final LootSplitGuildUploadHelper guildUploadHelperForText = new LootSplitGuildUploadHelper(text, lootSplitId, event.getGuild().getIdLong(), false);
+                guildUploadHelperForText.uploadGuildUsers();
+                guildUploadHelperForText.sendGuildUploadPaginator(event);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                sendErrorUploadMessage(event);
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+                sendErrorReadingMessage(event);
+                return;
+            }
+        } else if (event.getOption("file") != null && !event.getOption("file").getAsAttachment().isImage() && !event.getOption("file").getAsAttachment().isVideo()) {
+            // Get the text from the file
+            textFile = new File(event.getOption("file").getAsAttachment().getFileName().getBytes(StandardCharsets.UTF_8).toString());
+            cf = event.getOption("file").getAsAttachment().getProxy().downloadToFile(textFile).thenAccept(file -> {
+                // Send a confirmation message to Discord
+                System.out.println("File downloaded and read successfully!");
+            });
+
+            File finalTextFile = textFile;
+            cf.whenComplete((result, error) -> {
+                if (error != null) {
+                    System.out.println("Exception: " + error.getMessage());
+                } else {
+                    try {
+                        final String textFromFile = new String(Files.readAllBytes(finalTextFile.toPath()));
+                        final LootSplitGuildUploadHelper guildUploadHelperForFile = new LootSplitGuildUploadHelper(textFromFile, lootSplitId, event.getGuild().getIdLong(), true);
+                        guildUploadHelperForFile.uploadGuildUsers();
+                        guildUploadHelperForFile.sendGuildUploadPaginator(event);
+                        finalTextFile.delete();
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        sendErrorUploadMessage(event);
+                        return;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        sendErrorReadingMessage(event);
+                        return;
+                    }
+                }
+            });
+        } else {
+            sendNotGuildListMessage(event);
+            return;
         }
     }
-
-    /* Error Messages */
-
 }
