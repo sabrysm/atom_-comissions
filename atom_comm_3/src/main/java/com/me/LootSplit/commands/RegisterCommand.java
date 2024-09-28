3d167f9cd92a1f28389b1c77a8c53fc666d57b87
Abdelrahman Sabry <Drakth5364@gmail.com>
2024-09-28 16:04:28 +0300

    Register command now renames the nickname of the user in the server

--- atom_comm_3/src/main/java/com/me/LootSplit/commands/RegisterCommand.java
+++ atom_comm_3/src/main/java/com/me/LootSplit/commands/RegisterCommand.java
@@ -23,22 +23,27 @@ public class RegisterCommand implements ISlashCommand {
     @Override
     public void execute(@NotNull SlashCommandInteractionEvent event) {
         try {
+            event.deferReply(false).queue();
             String username = event.getOption("username").getAsString();
             DatabaseManager databaseManager = new DatabaseManager();
 
-            if (databaseManager.isUserExists(username)) {
-                sendUserAlreadyExistsMessage(event);
+            if (!databaseManager.isUserExists(username)) {
+                sendUserNotInGuildListMessage(event, username);
                 return;
             } else if (databaseManager.isUserRegistered(Long.parseLong(event.getUser().getId()))) {
                 sendUserAlreadyRegisteredMessage(event);
                 return;
             } else {
+                // Register the user in the database
                 databaseManager.registerTheUser(username, Long.parseLong(event.getUser().getId()));
+                // Change the user's nickname to the in-game username
+                event.getGuild().modifyNickname(event.getMember(), username).queue();
                 sendUserRegisteredSuccessfullyMessage(event, username);
             }
         } catch (SQLException e) {
             raiseSQLError("Could not register user");
         } catch (Exception e) {
+            event.getHook().sendMessage("Could not register user").queue();
             raiseUnknownError("Could not register user");
         }
     }
